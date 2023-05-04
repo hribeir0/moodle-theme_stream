62712b61ae722cd45870f684136229c764607c6e
hribeiro <hribeiro@uporto.pt>
2023-05-04 14:27:33 +0100

    version 1.0

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_stream';
 $plugin->release = '1.0';
-$plugin->version = 2023041000;
+$plugin->version = 2023041004;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
