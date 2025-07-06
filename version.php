298fd0d90382f45699fa37f98ed857cce95957c8
hribeir0 <ribeiro.hugo@gmail.com>
2025-07-06 10:42:53 +0100

    Fix version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_stream';
 $plugin->release = '1.6';
-$plugin->version = 2025041700;
+$plugin->version = 2025070600;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = [
